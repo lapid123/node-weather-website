4076955d863e32706d37b95828c5e3efd2cf1504
Lapid123 <omrilapid1@gmail.com>
2019-05-05 18:07:37 +0300

    Setup for heroku

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const hbs = require('hbs')
-
+const port = process.env.PORT || 3000
 // Define paths for express config
 const publicDirPath = path.join(__dirname,'../public')
 const viewsPath = path.join(__dirname,'../templates/views')
@@ -83,6 +83,7 @@ app.get('*',(req,res) => {
         errorMessage: 'Page not found!'
     })
 })
-app.listen(3000, ()=>{
-    console.log('Server is up on port 3000')
+
+app.listen(port, ()=>{
+    console.log('Server is up on port' + port)
 })
